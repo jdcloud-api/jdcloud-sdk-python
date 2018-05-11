de20ead6338142db8ede2f92948155224327ae97
Tanc009 <tancong@jd.com>
2018-05-11 10:52:34 +0000

    publish jdcloud-sdk-python

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README'), encoding='utf-8') as f:
 
 setup(
     name='jdcloud_sdk',
-    version="1.0.5",
+    version="1.0.6",
     description=long_description,
     author='JDCloud API Gateway Team',
     url='https://github.com/jdcloud-api/jdcloud-sdk-python',

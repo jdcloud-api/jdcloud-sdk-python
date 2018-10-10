95032807be5775029d031c092cec9efb6c2d03be
Tanc009 <tancong@jd.com>
2018-10-10 03:31:39 +0000

    publish jdcloud-sdk-python 1.2.16

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from os import path
 from codecs import open
 
 here = path.abspath(path.dirname(__file__))
-# Get the long description from the README file
+# Get the long description from the README.md file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='jdcloud_sdk',
-    version="1.2.15",
+    version="1.2.16",
     description=long_description,
     author='JDCloud API Gateway Team',
     url='https://github.com/jdcloud-api/jdcloud-sdk-python',

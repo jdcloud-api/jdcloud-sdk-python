f7de5827a1433afebf916b8b0bf748b08876f95d
Tanc009 <tancong@jd.com>
2019-07-11 19:57:10 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README.md file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='jdcloud_sdk',
+    version="1.6.3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='JDCloud API Gateway Team',
+    url='https://github.com/jdcloud-api/jdcloud-sdk-python',
+    scripts=[],
+    packages=find_packages(),
+    install_requires=['requests'],
+    license="Apache License V2.0"
+)

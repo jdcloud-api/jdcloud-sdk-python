b75f35330d72d83ceb4c34085951b657e41781d3
Tanc009 <tancong@jd.com>
2019-10-11 11:55:17 +0000

    publish jdcloud-sdk-python 1.6.17

--- jdcloud_sdk/services/elite/models/ProductServiceVo.py
+++ jdcloud_sdk/services/elite/models/ProductServiceVo.py
@@ -19,10 +19,13 @@
 
 class ProductServiceVo(object):
 
-    def __init__(self, deliverNumber=None, orderNumber=None, buyerPin=None, mappingCode=None, supportAutoDeliver=None, productId=None, skuId=None, skuName=None, serviceCode=None, num=None, period=None, periodType=None, periodTypeName=None, accountNum=None, deliverStatus=None, deliverStatusName=None, effectiveDt=None, failureDt=None, extraInfo=None, remark=None, orderTotalFee=None, orderActualFee=None, paymentDt=None, extraChargeInfo=None, orderItemExtraChargeInfos=None):
+    def __init__(self, deliverNumber=None, orderNumber=None, refOrderNumber=None, orderType=None, orderTypeName=None, buyerPin=None, mappingCode=None, supportAutoDeliver=None, productId=None, skuId=None, skuName=None, serviceCode=None, num=None, period=None, periodType=None, periodTypeName=None, accountNum=None, deliverStatus=None, deliverStatusName=None, effectiveDt=None, failureDt=None, extraInfo=None, remark=None, orderTotalFee=None, orderActualFee=None, paymentDt=None, extraChargeInfo=None, orderItemExtraChargeInfos=None):
         """
         :param deliverNumber: (Optional) 交付单号
         :param orderNumber: (Optional) 订单号
+        :param refOrderNumber: (Optional) 续费订单所关联的新购订单号，该字段只针对续费单有效，新购单该字段为null
+        :param orderType: (Optional) 订单类型
+        :param orderTypeName: (Optional) 订单类型名称
         :param buyerPin: (Optional) 购买人
         :param mappingCode: (Optional) 映射编号
         :param supportAutoDeliver: (Optional) 是否支持自动交付：0表示不支持，1表示支持
@@ -50,6 +53,9 @@ class ProductServiceVo(object):
 
         self.deliverNumber = deliverNumber
         self.orderNumber = orderNumber
+        self.refOrderNumber = refOrderNumber
+        self.orderType = orderType
+        self.orderTypeName = orderTypeName
         self.buyerPin = buyerPin
         self.mappingCode = mappingCode
         self.supportAutoDeliver = supportAutoDeliver
--- jdcloud_sdk/services/iothub/models/DataVO.py
+++ jdcloud_sdk/services/iothub/models/DataVO.py
@@ -19,7 +19,7 @@
 
 class DataVO(object):
 
-    def __init__(self, id=None, name=None, subNetId=None, subNetName=None, vpcId=None, vpcName=None, azName=None, pubDomain=None, priDomain=None, status=None, feetype=None, createTime=None, instanceId=None):
+    def __init__(self, id=None, name=None, subNetId=None, subNetName=None, vpcId=None, vpcName=None, azName=None, pubDomain=None, priDomain=None, status=None, createTime=None, endTime=None, maxDevices=None, maxMessage=None, instanceId=None, instanceType=None):
         """
         :param id: (Optional) 系统流水号
         :param name: (Optional) 实例名称
@@ -30,10 +30,13 @@ class DataVO(object):
         :param azName: (Optional) 可用区名称
         :param pubDomain: (Optional) 公网域名
         :param priDomain: (Optional) 内网域名
-        :param status: (Optional) 实例状态[0-创建中，1-运行中，2-停止]
-        :param feetype: (Optional) 资费形式
+        :param status: (Optional) IoT Hub实例状态[10~100为创建中,0或1-运行中,2-欠费停服,3-待删除]
         :param createTime: (Optional) 创建时间
+        :param endTime: (Optional) 实例到期时间
+        :param maxDevices: (Optional) 最大在线设备数
+        :param maxMessage: (Optional) 最大消息条数
         :param instanceId: (Optional) 实例编号
+        :param instanceType: (Optional) 实例类型[0-独享，1-共享]
         """
 
         self.id = id
@@ -46,6 +49,9 @@ class DataVO(object):
         self.pubDomain = pubDomain
         self.priDomain = priDomain
         self.status = status
-        self.feetype = feetype
         self.createTime = createTime
+        self.endTime = endTime
+        self.maxDevices = maxDevices
+        self.maxMessage = maxMessage
         self.instanceId = instanceId
+        self.instanceType = instanceType

0e5edb19d012bc131fb19411bb606d5884fd100b
jdcloud-apigateway <jdcloud-api@jd.com>
2021-10-15 10:47:30 +0000

    publish jdcloud-sdk-python 1.6.119

--- jdcloud_sdk/services/cloudauth/models/AccountInfo.py
+++ jdcloud_sdk/services/cloudauth/models/AccountInfo.py
@@ -19,19 +19,21 @@
 
 class AccountInfo(object):
 
-    def __init__(self, orgName, bankCardNum, bankName, branchBankName, bankCode=None, cityCode=None, provinceCode=None):
+    def __init__(self, orgName, bankCardNum, bankName, branchBankName, mobile=None, bankCode=None, cityCode=None, provinceCode=None):
         """
-        :param orgName:  组织名称
-        :param bankCardNum:  银行卡号
+        :param orgName:  机构名称
+        :param bankCardNum:  企业银行账户
+        :param mobile: (Optional) 企业手机号
         :param bankName:  银行名称
-        :param branchBankName:  支行名称
-        :param bankCode: (Optional) 银行代码
-        :param cityCode: (Optional) 城市代码
-        :param provinceCode: (Optional) 省份代码
+        :param branchBankName:  开户行所在分行网点名称
+        :param bankCode: (Optional) 开户行所在的银行编码
+        :param cityCode: (Optional) 开户行所在的城市编码
+        :param provinceCode: (Optional) 开户行所在的省份编码
         """
 
         self.orgName = orgName
         self.bankCardNum = bankCardNum
+        self.mobile = mobile
         self.bankName = bankName
         self.branchBankName = branchBankName
         self.bankCode = bankCode

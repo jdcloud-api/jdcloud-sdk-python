03010ca33e6bf30cd1d1f2dff25da674145cd0b4
jdcloud-apigateway <jdcloud-api@jd.com>
2022-06-30 08:22:15 +0000

    publish jdcloud-sdk-python 1.6.167

--- jdcloud_sdk/services/cdn/client/CdnClient.py
+++ jdcloud_sdk/services/cdn/client/CdnClient.py
@@ -26,4 +26,4 @@ class CdnClient(JDCloudClient):
         if config is None:
             config = Config('cdn.jdcloud-api.com')
 
-        super(CdnClient, self).__init__(credential, config, 'cdn', '0.10.31', logger)
+        super(CdnClient, self).__init__(credential, config, 'cdn', '0.10.32', logger)

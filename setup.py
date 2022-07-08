59c7a966c7496ad49407e5ed9f60c6b2bcbb5903
jdcloud-apigateway <jdcloud-api@jd.com>
2022-07-08 03:48:25 +0000

    publish jdcloud-sdk-python 1.6.169

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='jdcloud_sdk',
-    version="1.6.168",
+    version="1.6.169",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='JDCloud API Gateway Team',
